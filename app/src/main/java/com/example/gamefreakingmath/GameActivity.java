abd2b502bf68ceac03c5f5a3cd9f5c9465202c77
Milkyway <duchuy96itc@gmail.com>
2021-04-13 09:48:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gamefreakingmath/GameActivity.java
@@ -0,0 +1,108 @@
+package com.example.gamefreakingmath;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView txt_score, txt_cal, dia_score;
+    ImageButton btn_true, btn_false, btn_close;
+    Random random;
+    int a, b, c, score = 0;
+    Dialog dialog;
+    Button btn_back, btn_playagain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        init();
+
+        random = new Random();
+        GenerateRandom();
+        for (int i = 0; i < 10; i++) {
+            GenerateRandom();
+            Log.d("test", "GenerateRandom: " + a + "+" + b + "=" + c);
+        }
+
+        btn_true.setOnClickListener(this);
+        btn_false.setOnClickListener(this);
+        btn_close.setOnClickListener(this);
+
+        btn_back.setOnClickListener(this);
+        btn_playagain.setOnClickListener(this);
+    }
+
+    private void init() {
+        txt_score = findViewById(R.id.score);
+        txt_cal = findViewById(R.id.calculate);
+        btn_close = findViewById(R.id.btn_close);
+        btn_true = findViewById(R.id.btn_true);
+        btn_false = findViewById(R.id.btn_false);
+        dialog = new Dialog(GameActivity.this);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.setCanceledOnTouchOutside(false);
+        btn_back = dialog.findViewById(R.id.btn_back);
+        btn_playagain = dialog.findViewById(R.id.btn_playagain);
+        dia_score = dialog.findViewById(R.id.content);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void GenerateRandom() {
+        a = random.nextInt(10);
+        b = random.nextInt(10);
+        c = random.nextInt((a + b) - (a + b - 1) + 1) + (a + b); // max = a+b, min = a+b-1
+        txt_cal.setText(a + " + " + b + " = " + c);
+    }
+
+    @SuppressLint({"NonConstantResourceId", "SetTextI18n"})
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_close:
+                finish();
+                break;
+            case R.id.btn_true:
+                if ((a + b) == c) {
+                    score++;
+                    txt_score.setText(score + "");
+                    GenerateRandom();
+                } else {
+                    dia_score.setText("Your score: " + score);
+                    dialog.show();
+                }
+                break;
+            case R.id.btn_false:
+                if ((a + b) != c) {
+                    score++;
+                    txt_score.setText(score + "");
+                    GenerateRandom();
+                } else {
+                    dia_score.setText("Your score: " + score);
+                    dialog.show();
+                }
+                break;
+            case R.id.btn_back:
+                dialog.hide();
+                finish();
+                break;
+            case R.id.btn_playagain:
+                dialog.hide();
+                txt_score.setText(0 + "");
+                score = 0;
+                GenerateRandom();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gamefreakingmath/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gamefreakingmath;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_play, btn_close;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        btn_play.setOnClickListener(this);
+    }
+
+    private void init() {
+        btn_play = findViewById(R.id.btn_play);
+        btn_close = findViewById(R.id.btn_close);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_play:
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+                break;
+            case R.id.btn_close:
+                finish();
+                break;
+        }
+    }
+}
